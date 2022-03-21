473f54b3c034e4652d0b4b52a7adea1458a898b7
GY-Jeong <jyjg1124@gmail.com>
2022-03-21 19:17:31 +0900

    initial commit

---
+++ src/scripts/makeCollection.java
@@ -0,0 +1,25 @@
+package scripts;
+
+/**
+ * 2주차 실습 코드
+ * 
+ * 주어진 5개의 html 문서를 전처리하여 하나의 xml 파일을 생성하세요. 
+ * 
+ * input : data 폴더의 html 파일들
+ * output : collection.xml 
+ */
+
+public class makeCollection {
+	
+	private String data_path;
+	private String output_flie = "./collection.xml";
+	
+	public makeCollection(String path) {
+		this.data_path = path;
+	}
+	
+	public void makeXml(){
+
+    }
+	
+}
---
+++ src/scripts/makeKeyword.java
@@ -0,0 +1,29 @@
+package scripts;
+
+/**
+ * 3주차 실습 코드
+ * 
+ * kkma 형태소 분석기를 이용하여 index.xml 파일을 생성하세요.
+ * 
+ * index.xml 파일 형식은 아래와 같습니다.
+ * (키워드1):(키워드1에 대한 빈도수)#(키워드2):(키워드2에 대한 빈도수)#(키워드3):(키워드3에 대한 빈도수) ... 
+ * e.g., 라면:13#밀가루:4#달걀:1 ...
+ * 
+ * input : collection.xml
+ * output : index.xml 
+ */
+
+public class makeKeyword {
+
+	private String input_file;
+	private String output_flie = "./index.xml";
+	
+	public makeKeyword(String file) {
+		this.input_file = file;
+	}
+
+	public void convertXml() {
+
+	}
+
+}
